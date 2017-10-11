723ba25fe276189b2b641490a469db90588c0239
Nick Brooks <nbrooks@thirtyseven.com.au>
2017-10-11 14:45:09 +1100

    Default Data

---
+++ client/data/index.js
@@ -0,0 +1,77 @@
+export const bathrooms = [
+    { value: 1 },
+    { value: 2 },
+    { value: 3 }
+]
+
+export const bedrooms = [
+    { value: "Studio" },
+    { value: 1 },
+    { value: 2 },
+    { value: 3 }
+]
+
+export const currencies = [
+    "AUD",
+    "BGN",
+    "BRL",
+    "CAD",
+    "CHF",
+    "CNY",
+    "CZK",
+    "DKK",
+    "GBP",
+    "HKD",
+    "HRK",
+    "HUF",
+    "IDR",
+    "ILS",
+    "INR",
+    "JPY",
+    "KRW",
+    "MXN",
+    "MYR",
+    "NOK",
+    "NZD",
+    "PHP",
+    "PLN",
+    "RON",
+    "RUB",
+    "SEK",
+    "SGD",
+    "THB",
+    "TRY",
+    "USD",
+    "ZAR"
+]
+
+export const kitchen = [
+    { value: "None" },
+    { value: "Shared" },
+    { value: "Included" }
+]
+
+export const rentalLengths = [
+    {
+        length: 1,
+        value: "one_month"
+    },
+    {
+        length: 3,
+        value: "three_months"
+    },
+    {
+        length: 6,
+        value: "six_months"
+    }
+]
+
+export const rentalType = [
+    { value: "Hotel" },
+    { value: "Hostel" },
+    { value: "Guesthouse" },
+    { value: "House" },
+    { value: "Villa" },
+    { value: "Coliving" },
+    { value: "Condo" }
+]
