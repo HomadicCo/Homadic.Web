fadd4dcea4b1ac0bc8c2789fbf7edd0219a41fd5
Nick Brooks <nbrooks@thirtyseven.com.au>
2017-11-03 00:11:22 +0700

    Done notes

--- client/store/AddListing/Steps/Notes.js
+++ client/store/AddListing/Steps/Notes.js
@@ -7,19 +7,24 @@ class Notes extends React.Component {
         super(props)
 
         this.handleNextClick = this.handleNextClick.bind(this);
+        this.handlePrevClick = this.handlePrevClick.bind(this);
         this.handleChange = this.handleChange.bind(this);
     }
 
     handleNextClick(e) {
         e.preventDefault();
-        const form = this.refs.listingForm;
-        console.log(form);
+        const form = this.refs.notesForm;
 
         if (form.checkValidity() == false) {
             form.classList.add("was-validated");
         } else {
             browserHistory.push("/add/preview");
         }
+    }
+
+    handlePrevClick(e) {
+        e.preventDefault();
+        browserHistory.push("/add/amenities");
     }
 
     handleChange(e) {
@@ -41,6 +46,12 @@ class Notes extends React.Component {
                 <h1 className="fancy blue display-4 mb-4">Additional notes</h1>
                 <textarea id="inputNotes" className="form-control" placeholder="Any additional notes..." name="notes" value={listing.notes} onChange={this.handleChange} />
                 <small id="emailHelp" className="form-text text-muted">This should mention any notable features or things to know, not your personal review. Reviews are coming soon!</small>
+                <div className="row justify-content-center mt-4">
+                    <div className="col-auto">
+                        <button type="button" onClick={this.handlePrevClick} className="btn btn-outline-success mx-1"><FontAwesome name="caret-left" /> Amenities</button>
+                        <button type="button" onClick={this.handleNextClick} className="btn btn-success mx-1">Preview <FontAwesome name="caret-right" /></button>
+                    </div>
+                </div>
             </form>
         )
     }
