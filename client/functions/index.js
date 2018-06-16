3f67d02394dde8b23e6b71d00d4dcb17a24e2d2b
Nick Brooks <nbrooks@thirtyseven.com.au>
2018-06-16 17:38:57 -0600

    Remove hardcoded hostname

--- client/functions/index.js
+++ client/functions/index.js
@@ -119,7 +119,7 @@ export function extractYoutubeFromString(string) {
 
 // generate the facebook redirect Url
 export function getLoginUrl(state) {
-    const redirectUri = 'http://localhost:9990/signin-facebook';
+    const redirectUri = window.location.hostname + '/signin-facebook';
     return 'https://www.facebook.com/v2.10/dialog/oauth?client_id=812498655591761&scope=email&redirect_uri=' + redirectUri + '&state=' + state;
 }
 
