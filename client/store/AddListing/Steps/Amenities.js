16ab258aefc95517a997773218a0a531d24d6f70
Nick Brooks <nbrooks@thirtyseven.com.au>
2018-06-20 08:52:04 -0600

    Added speedtest

--- client/store/AddListing/Steps/Amenities.js
+++ client/store/AddListing/Steps/Amenities.js
@@ -98,27 +98,34 @@ class Amenities extends React.Component {
                 </div>
 
                 {listing.wifi.type != 'none' ?
-                    <div className="form-row">
-                        {(['paid', 'canInstall'].indexOf(listing.wifi.type) > -1) ?
+                    <div>
+                        <div className="form-row">
+                            {(['paid', 'canInstall'].indexOf(listing.wifi.type) > -1) ?
+                                <div className="form-group col-md-3">
+                                    <label htmlFor="inputInternetCost" className="col-form-label mr-2">Cost per month <i className="text-muted far fa-dollar" /></label>
+                                    <div className="input-group mb-2 mr-sm-2 mb-sm-0">
+                                        <div className="input-group-addon">{listing.currency}</div>
+                                        <input type="text" className="form-control" id="inputInternetCost" data-type="int" name="wifi.rate" value={listing.wifi.rate} onChange={this.handleChange} />
+                                    </div>
+                                </div> : undefined}
                             <div className="form-group col-md-3">
-                                <label htmlFor="inputInternetCost" className="col-form-label mr-2">Cost per month <i className="text-muted far fa-dollar" /></label>
+                                <label htmlFor="inputInternetDownload" className="col-form-label mr-2">Download <i className="text-muted fas fa-cloud-download-alt" /></label>
                                 <div className="input-group mb-2 mr-sm-2 mb-sm-0">
-                                    <div className="input-group-addon">{listing.currency}</div>
-                                    <input type="text" className="form-control" id="inputInternetCost" data-type="int" name="wifi.rate" value={listing.wifi.rate} onChange={this.handleChange} />
+                                    <input type="number" className="form-control" id="inputInternetDownload" data-type="int" max={100} name="wifi.download" value={listing.wifi.download} onChange={this.handleChange} />
+                                    <div className="input-group-addon">mbps</div>
                                 </div>
-                            </div> : undefined}
-                        <div className="form-group col-md-3">
-                            <label htmlFor="inputInternetDownload" className="col-form-label mr-2">Download <i className="text-muted fas fa-cloud-download-alt" /></label>
-                            <div className="input-group mb-2 mr-sm-2 mb-sm-0">
-                                <input type="number" className="form-control" id="inputInternetDownload" data-type="int" max={100} name="wifi.download" value={listing.wifi.download} onChange={this.handleChange} />
-                                <div className="input-group-addon">mbps</div>
+                            </div>
+                            <div className="form-group col-md-3">
+                                <label htmlFor="inputInternetUpload" className="col-form-label mr-2">Upload <i className="text-muted fas fa-cloud-upload-alt" /></label>
+                                <div className="input-group mb-2 mr-sm-2 mb-sm-0">
+                                    <input type="number" className="form-control" id="inputInternetUpload" data-type="int" max={100} name="wifi.upload" value={listing.wifi.upload} onChange={this.handleChange} />
+                                    <div className="input-group-addon">mbps</div>
+                                </div>
                             </div>
                         </div>
-                        <div className="form-group col-md-3">
-                            <label htmlFor="inputInternetUpload" className="col-form-label mr-2">Upload <i className="text-muted fas fa-cloud-upload-alt" /></label>
-                            <div className="input-group mb-2 mr-sm-2 mb-sm-0">
-                                <input type="number" className="form-control" id="inputInternetUpload" data-type="int" max={100} name="wifi.upload" value={listing.wifi.upload} onChange={this.handleChange} />
-                                <div className="input-group-addon">mbps</div>
+                        <div className="form-row">
+                            <div className="form-group col-xs-12">
+                                <a href="http://www.speedtest.net/?ref=https:/homadic.co" className="btn btn-action btn-sm" rel="noopener noreferrer" target="_blank"><i className="fas fa-rocket"></i> SpeedTest</a>
                             </div>
                         </div>
                     </div> : undefined}
